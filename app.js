be04b7b5f148de08b21b2e90649b69aa91a6e607
Amir Saghaleini <EEDEA.Media.Group@gmail.com>
2022-12-17 20:23:08 +0330

    4.2 Blog list, step2 - app

---
+++ app.js
@@ -0,0 +1,29 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const notesRouter = require("./controllers/blogs");
+// const {requestLogger,unknownEndpoint,errorHandler} = require('./utils/middleware')
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose
+  .set("strictQuery", false)
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+// app.use(requestLogger)
+
+app.use("/api/blogs", notesRouter);
+
+// app.use(unknownEndpoint)
+// app.use(errorHandler)
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 const http = require("http");
-const express = require("express");
-const app = express();
-const blogsRouter = require("./controllers/blogs");
-
-const cors = require("cors");
-const mongoose = require("mongoose");
-
+const app = require("./app");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
 
-const mongoUrl = config.MONGODB_URI;
-mongoose
-  .set("strictQuery", false)
-  .connect(mongoUrl)
-  .then(() => {
-    logger.info("Connected to MongoDB");
-  });
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", blogsRouter);
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
