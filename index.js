d026b50320568846086b79b9212520b7ddbdca5b
Amir Saghaleini <EEDEA.Media.Group@gmail.com>
2022-12-17 20:00:55 +0330

    4.2 Blog list, step2 - blogRouter

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+const blogsRouter = require("./controllers/blogs");
+
 const cors = require("cors");
 const mongoose = require("mongoose");
 
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-const Blog = require("./models/blog");
 
 const mongoUrl = config.MONGODB_URI;
 mongoose
@@ -19,19 +20,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
