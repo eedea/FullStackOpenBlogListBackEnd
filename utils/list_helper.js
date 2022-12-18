6148e350dac14d4b09ac0e9001e6eb4cd5780026
Amir Saghaleini <EEDEA.Media.Group@gmail.com>
2022-12-18 18:48:31 +0330

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,16 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return;
+  return blogs.reduce((favorite, blog) =>
+    blog.likes > favorite.likes ? blog : favorite
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
